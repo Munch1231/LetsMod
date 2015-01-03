96e6c4ae3b3923930fba41fb22ed197161f2eede
Kyle <khawley1231@gmail.com>
2015-01-02 20:17:28 -0500

    Finished up the resources folder!

---
+++ src/main/java/com/Munch1231/letsmodreboot/LetsModReboot.java
@@ -0,0 +1,32 @@
+package com.Munch1231.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "LetsModReboot", name = "Lets Mod Reboot", version = "1.7.2-1.0")
+public class LetsModReboot
+{
+    @Mod.Instance("LetsModReboot")
+    public static LetsModReboot instance;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event)
+    {
+
+    }
+}
